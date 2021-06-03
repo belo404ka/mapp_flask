9b730e5775dab8f16c6f80120b60126971106608
belo404ka <68505094+belo404ka@users.noreply.github.com>
2021-06-03 17:33:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import json
 import h3
 from flask import request
 import json
-import requests
 import plotly.express as px
 import polyfill
-from geojson import Polygon
+import pandas as pd
+from geojson import Feature, Point, FeatureCollection
 from pandas.io.json import json_normalize
 import dash_html_components as html
 import dash_core_components as dcc
@@ -26,16 +26,9 @@ lon = 83.104486
 z = 12.79
 
 
-
-maps = f'https://tile0.maps.2gis.com/tiles?x={lat}&y={lon}&z={z}'
-
-request = urllib.request.Request(maps)
-request.add_header('Content-Type','application/json')
-response = urllib.request.urlopen(request)
-
-decodedRes = response.read().decode('utf-8')
-json_object = json.load(decodedRes)
-df = json_normalize(json_object)
+df = pd.read_csv("coordinates.csv",
+                   dtype={"id": str})
+# maps = f'https://tile0.maps.2gis.com/tiles?x={lat}&y={lon}&z={z}'
 
 
 
@@ -47,23 +40,25 @@ def get_coordinates():
 def get_polylines():
     raw_data = get_coordinates()
     hexagons = []
-    for rd in raw_data:
-        geoJson = dict(type="Polygon", coordinates=[rd])
-        hexagons.extend(h3.polyfill(geoJson, 10))
+    lat = []
+    lng = []
+    for rd in raw_data['features']:
+        geoJson = dict(type="Polygon", coordinates=rd['geometry']['coordinates'])
+        hexagons = list((h3.polyfill(geoJson, 10)))
     polylines = []
     for hex in hexagons:
         polygons = h3.h3_set_to_multi_polygon([hex], geo_json=False)
         outlines = [loop for polygon in polygons for loop in polygon]
         polyline = [outline + [outline[0]] for outline in outlines][0]
-        polylines.append(polyline)
-    return Polygon(polylines)
-
+        lat.extend(map(lambda v: v[0], polyline))
+        lng.extend(map(lambda v: v[1], polyline))
+        polylines.append(Feature(polyline))
+    return FeatureCollection(polylines)
 
 
 def get_figure():
-    fig = px.choropleth_mapbox(df, geojson=get_polylines(), locations=df.location, center={"lat": df.lat, "lon": df.lon},
-                               color='',
-                               featureidkey=df.location
+    fig = px.choropleth_mapbox(df, geojson=get_polylines(), locations=df.id,
+                               color=df.color, mapbox_style="carto-positron",
                                )
     # Set templates
 
