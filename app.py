2798b098fbbba9bbc41308adb2611ab39e653556
belo404ka <68505094+belo404ka@users.noreply.github.com>
2021-06-04 16:09:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import json
 import plotly.express as px
 import polyfill
 import pandas as pd
-from geojson import Feature, Point, FeatureCollection
+from geojson import Feature, Point, FeatureCollection, Polygon
 from pandas.io.json import json_normalize
 import dash_html_components as html
 import dash_core_components as dcc
@@ -26,8 +26,7 @@ lon = 83.104486
 z = 12.79
 
 
-df = pd.read_csv("coordinates.csv",
-                   dtype={"id": str})
+df = pd.read_csv("coordinates.csv", dtype={"id": str})
 # maps = f'https://tile0.maps.2gis.com/tiles?x={lat}&y={lon}&z={z}'
 
 
@@ -39,28 +38,45 @@ def get_coordinates():
 
 def get_polylines():
     raw_data = get_coordinates()
-    hexagons = []
-    lat = []
-    lng = []
+    list_features = []
     for rd in raw_data['features']:
-        geoJson = dict(type="Polygon", coordinates=rd['geometry']['coordinates'])
-        hexagons = list((h3.polyfill(geoJson, 10)))
-    polylines = []
-    for hex in hexagons:
-        polygons = h3.h3_set_to_multi_polygon([hex], geo_json=False)
-        outlines = [loop for polygon in polygons for loop in polygon]
-        polyline = [outline + [outline[0]] for outline in outlines][0]
-        lat.extend(map(lambda v: v[0], polyline))
-        lng.extend(map(lambda v: v[1], polyline))
-        polylines.append(Feature(polyline))
-    return FeatureCollection(polylines)
+        len_coor = len(rd['geometry']['coordinates'][0])
+        for i in range(len_coor):
+            lat = rd['geometry']['coordinates'][0][i][0]
+            lng = rd['geometry']['coordinates'][0][i][1]
+            a = h3.geo_to_h3(lat, lng, 10)
+            points = Polygon([h3.h3_to_geo_boundary(a, True)])
+            feature = Feature(geometry=points,
+                              id=raw_data['features'][0]['id'],
+                              )
+            list_features.append(feature)
+    return FeatureCollection(list_features)
+
+
+
+
+
+    #     geoJson = dict(type="Polygon", coordinates=rd['geometry']['coordinates'])
+    #     hexagons = list((h3.polyfill(geoJson, 10)))
+    # polylines = []
+    # for hex in hexagons:
+    #     polygons = h3.h3_set_to_multi_polygon([hex], geo_json=False)
+    #     outlines = [loop for polygon in polygons for loop in polygon]
+    #     polyline = [outline + [outline[0]] for outline in outlines][0]
+    #     lat.extend(map(lambda v: v[0], polyline))
+    #     lng.extend(map(lambda v: v[1], polyline))
+    #     polylines.append(Feature(polyline))
+    # return FeatureCollection(polylines)
 
 
 def get_figure():
     fig = px.choropleth_mapbox(df, geojson=get_polylines(), locations=df.id,
                                color=df.color, mapbox_style="carto-positron",
+                               zoom=3, center={"lat": 54.748079, "lon": 83.104486},
                                )
     # Set templates
+
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 
     app.layout = html.Div([
         dcc.Graph(
